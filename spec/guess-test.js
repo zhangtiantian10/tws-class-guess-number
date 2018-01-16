1d34ffba96fb6e3ec207364f248c12a1e249241c
zhang tian <975571814@qq.com>
2018-01-16 23:54:10 +0800

    modify class Guess test
    Signed-off-by: zhang tian <975571814@qq.com>

--- spec/guess-test.js
+++ spec/guess-test.js
@@ -1,13 +1,10 @@
-const sinon = require('sinon')
 const Guess = require('../src/guess')
-const AnswerGenerator = require('../src/answer-generator')
 const CompareNumber = require('../src/compare-number')
 
 describe('', () => {
 	it('', () => {
-		const answerGenerator = new AnswerGenerator()
-		sinon.stub(answerGenerator, "getAnswer").returns('1234')
-		const guess = new Guess(answerGenerator, new CompareNumber())
+		const answer = '1234'
+		const guess = new Guess(answer, new CompareNumber())
 		const input = '1234';
 		expect(guess.combine(input)).toEqual('4A0B');
 	})
